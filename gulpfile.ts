58f8c98a749796c9387ffa0e06791b5203459298
Joseph Briggs <jbriggsr3@gmail.com>
2021-02-24 18:52:01 +0000

    Setup Package

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,7 +1,5 @@
 import { series, src, watch } from 'gulp'
 import * as GulpSSH from 'gulp-ssh'
-import * as rimraf from 'rimraf'
-import run from 'gulp-run-command'
 
 const config = {
   remoteFolderPath: '/home/pi/vl53l0x',
@@ -17,15 +15,6 @@ const SSH = new GulpSSH({
   ignoreErrors: false,
   sshConfig: config.ssh,
 })
-
-const deployLibTask = () => src('lib/**').pipe(SSH.dest(`${config.remoteFolderPath}`))
-const deployPackage = () => src(['package.json', 'package-lock.json']).pipe(SSH.dest(`${config.remoteFolderPath}`))
-const npmInstallDeps = () => SSH.shell([`cd ${config.remoteFolderPath}`, `npm install --only=production`])
-
-const runTSCTask = async () => run('npm run tsc')()
-const cleanup = (cb) => rimraf('lib', { force: true }, cb)
-export const depsTask = series(deployPackage, npmInstallDeps)
-export const buildLibTask = series(cleanup, runTSCTask)
 
 const cleanupDev = (cb) => SSH.shell([`cd ${config.remoteFolderPath}`, `find -maxdepth 1 ! -name "tsconfig.json" ! -name "package.json" ! -name "package-lock.json" ! -name "node_modules" ! -name . -exec rm -rv {} \;`, `ls -lah`])
 const deployDevTask = () => src('src/**').pipe(SSH.dest(`${config.remoteFolderPath}/src`))
